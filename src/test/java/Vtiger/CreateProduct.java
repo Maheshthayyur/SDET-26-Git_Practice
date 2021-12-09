8f7995eb157d7be8b6303d7d226370546919ea20
sameer <sameer@Bullet-Sameer1998>
2021-12-09 13:50:19 +0530

    Test Product Page Added

---
+++ src/test/java/Vtiger/CreateProduct.java
@@ -0,0 +1,12 @@
+package Vtiger;
+
+import org.testng.annotations.Test;
+
+public class CreateProduct {
+	@Test
+	public void createProduct()
+	{
+		System.out.println("this is user-1 , scrip-1");
+	}
+
+}
